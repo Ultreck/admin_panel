62f0b36ccb0eb590ba0618773eaa88ad79ebe897
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-06-25 05:38:52 +0100

    feat: Added vercel json file

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { queryClient } from "./lib/queryClient";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "./components/ui/toaster";
 
-import Landing from "@/pages/landing";
-import Dashboard from "@/pages/dashboard";
-import NotFound from "@/pages/not-found";
+import Landing from "./pages/landing";
+import Dashboard from "./pages/dashboard";
+import NotFound from "./pages/not-found";
 // import { ProtectedRoute } from "./routes/ProtectedRoute";
 
 function App() {
