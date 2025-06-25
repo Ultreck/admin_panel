973f4e888ca80a7dcaf8ba079c82dc82fbfd07d2
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-06-25 05:21:06 +0100

    feat: Added vercel json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+// App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
-// import { Toaster } from "@/components/ui/toaster";
+import { queryClient } from "./lib/queryClient";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+
 import Landing from "@/pages/landing";
 import Dashboard from "@/pages/dashboard";
 import NotFound from "@/pages/not-found";
-import { Toaster } from "./components/ui/toaster";
 import { ProtectedRoute } from "./routes/ProtectedRoute";
-import { BrowserRouter } from "react-router-dom";
-// import { Login } from "./pages/Login";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Landing} />
-      <Route path="/dashboard">
-        <ProtectedRoute>
-          <Dashboard />
-        </ProtectedRoute>
-      </Route>
-
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
@@ -32,7 +16,18 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </TooltipProvider>
       </QueryClientProvider>
     </BrowserRouter>
